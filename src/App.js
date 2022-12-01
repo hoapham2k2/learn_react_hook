05dab8c1881d229a73622e95c3c51d7427c4bcab
hoapham <20520995@gm.uit.edu.vn>
2022-12-01 12:51:56 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Bai1 from "./Bai1/Bai1";
-import Bai2 from "./Bai2/Bai2";
-import Bai3 from "./Bai3/Bai3";
-
 function App() {
-  return (
-    <div className="App">
-      {/* bai1
-      <Bai1 /> */}
-      {/* bai2
-      <Bai2 /> */}
-      {/* bai3
-      <Bai3 /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
